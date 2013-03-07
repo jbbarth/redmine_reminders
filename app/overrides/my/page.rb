4e6a03d88cdf705d68c6f7fdf0ea87e6a54ebbde
jbbarth <jeanbaptiste.barth@gmail.com>
2013-03-07 09:01:11 +0100

    I hate single quotes more and more, don't know why :/

--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -1,9 +1,9 @@
-Deface::Override.new :virtual_path  => 'my/page',
-                     :name          => 'add-new-reminder-link-to-my-page',
-                     :insert_top    => 'div.contextual' do
+Deface::Override.new :virtual_path  => "my/page",
+                     :name          => "add-new-reminder-link-to-my-page",
+                     :insert_top    => "div.contextual" do
   %(
     <% content_for :header_tags do %>
-      <%= stylesheet_link_tag 'reminders', :plugin => "redmine_reminders" %>
+      <%= stylesheet_link_tag "reminders", :plugin => "redmine_reminders" %>
     <% end %>
     <%= link_to l(:label_add_reminder), new_reminder_path, :class => "icon icon-reminder", :id => "new-reminder" %>
   )
