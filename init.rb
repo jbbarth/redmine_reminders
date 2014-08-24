4012a6795bb975b39ec5ec2dbe1d65f068ffb8d0
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-24 15:34:31 +0200

    Depend on redmine_base_deface and remove dirty hacks for deface integration

--- init.rb
+++ init.rb
@@ -7,16 +7,9 @@ Redmine::Plugin.register :redmine_reminders do
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_reminders'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
   project_module :reminders do
     permission :view_reminders, { :reminders => [:index] }
     permission :manage_reminders, { :reminders => [:new, :create, :edit, :update, :destroy] }
   end
 end
-
-# Little hack for using the 'deface' gem in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides in app/overrides/ are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-# TODO: see if it's complicated to turn a plugin into a Railtie or find something a bit cleaner
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
