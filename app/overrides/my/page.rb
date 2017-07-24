63fb726231a6ae5fd1a36e577ec81f48140014c0
Vincent Robert <vincent.robert@nanego.com>
2017-07-24 12:07:38 +0200

    Temporarily add override to custom my-page

--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -1,8 +1,4 @@
-Deface::Override.new :virtual_path  => "my/page",
-                     :name          => "add-new-reminder-link-to-my-page",
-                     :original      => "bc6ae6262eef79aab70c151bfacde1eb8e66512f",
-                     :insert_top    => "div.contextual" do
-  %(
+link_to_new_reminder = %(
     <% content_for :header_tags do %>
       <%= stylesheet_link_tag "reminders", :plugin => "redmine_reminders" %>
     <% end %>
@@ -10,10 +6,26 @@ Deface::Override.new :virtual_path  => "my/page",
                 :class => "icon icon-reminder",
                 :id => "new-reminder" if User.current.allowed_to?(:manage_reminders, nil, :global => true) %>
   )
-end
+
+
+Deface::Override.new :virtual_path  => "my/page",
+                     :name          => "add-new-reminder-link-to-my-page",
+                     :original      => "bc6ae6262eef79aab70c151bfacde1eb8e66512f",
+                     :insert_top    => "div.contextual",
+                     :text          => link_to_new_reminder
+Deface::Override.new :virtual_path  => "my/custom_page",
+                     :name          => "add-new-reminder-link-to-my-custom-page",
+                     :original      => "bc6ae6262eef79aab70c151bfacde1eb8e66512f",
+                     :insert_top    => "div.contextual",
+                     :text          => link_to_new_reminder
 
 Deface::Override.new :virtual_path  => "my/page",
                      :name          => "add-visible-reminders-on-my-page",
+                     :original      => "78c1f1aeece8df938d731e6cc8561d73839a508b",
+                     :insert_top    => "div.splitcontentright",
+                     :partial       => "reminders/boxes"
+Deface::Override.new :virtual_path  => "my/custom_page",
+                     :name          => "add-visible-reminders-on-my-custom-page",
                      :original      => "78c1f1aeece8df938d731e6cc8561d73839a508b",
                      :insert_top    => "div.splitcontentright",
                      :partial       => "reminders/boxes"
