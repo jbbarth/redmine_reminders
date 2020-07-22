22000678c8715f8f8cfedbb43a88ee8949d0fd78
Vincent Robert <vincent.robert@nanego.com>
2020-07-22 10:37:51 +0200

    Fix reminders integration in my-page

--- spec/system/reminders_on_my_page_spec.rb
+++ spec/system/reminders_on_my_page_spec.rb
@@ -1,33 +1,21 @@
 require "spec_helper"
 require File.expand_path('../../../app/overrides/my/page', __FILE__)
 
-describe "RemindersOnMyPage", type: :integration do
+describe "RemindersOnMyPage", type: :system do
+
   fixtures :projects, :trackers, :issue_statuses, :issues,
            :enumerations, :users, :issue_categories,
            :projects_trackers, :roles, :member_roles,
            :members, :enabled_modules, :workflows
 
-  before do
-    @controller = MyController.new
-    @request    = ActionDispatch::TestRequest.create
-    @response   = ActionDispatch::TestResponse.new
-    User.current = nil
-    @request.session[:user_id] = 1 # admin
-  end
-
-  #TODO: make test work ; seems deface overrides are not included in test environment (?)
   it "should see current reminders on /my/page" do
-=begin
     Reminder.create!(:text => "Should see", :user_id => 1, :start_at => 2.days.ago, :end_at => 2.days.from_now)
     Reminder.create!(:text => "Should not see", :user_id => 1, :start_at => "2000-03-01", :end_at => "2000-03-03")
     log_user("admin", "admin")
-    get "/my/page"
-    assert_response :success
-    binding.pry
-    assert_select "p.blah", :count => 1
-    assert_select "div.reminder", :count => 1
-  end
-=end
+    visit "/my/page"
+    expect(page).to have_selector("#my-page")
+    expect(page).to have_selector("div.reminder", text: "Should see")
+    expect(page).to_not have_selector("div.reminder", text: "Should not see")
   end
 
 end
